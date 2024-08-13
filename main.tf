31aa2ade21c3c34852b8d24d87328f14dd227a37
DarylvdBerg <darylvdberg@gmail.com>
2024-08-13 20:27:55 +0200

    Correct var name

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "devops_projects" {
   for_each = var.project_configuration
 
   devops_project_name                           = each.value.devops_project_name
-  devops_project_wiki_pages_list                     = each.value.devops_project_pages_list
+  devops_project_wiki_pages_list                = each.value.devops_project_pages_list
   devops_project_description                    = each.value.project_description
   devops_project_scrum_template                 = each.value.project_scrum_template
   devops_project_repository_git_permissions     = each.value.project_git_permissions
